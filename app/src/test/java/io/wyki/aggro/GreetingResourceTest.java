7fff0e51e3da664518f2a88a6f86b67ef23c26e4
Wykiki <jmegaplay@hotmail.fr>
2020-04-05 14:01:41 +0200

    Add test coverage

--- app/src/test/java/io/wyki/aggro/GreetingResourceTest.java
+++ app/src/test/java/io/wyki/aggro/GreetingResourceTest.java
@@ -1,12 +1,14 @@
 package io.wyki.aggro;
 
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
+@Tag("integration")
 public class GreetingResourceTest {
 
     @Test

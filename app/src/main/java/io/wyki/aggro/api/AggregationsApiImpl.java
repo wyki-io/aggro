28bec5102756c3246707ab336bf79fbf2ab626fa
Wykiki <jmegaplay@hotmail.fr>
2020-04-05 19:55:53 +0200

    Jacoco pre-rework

--- app/src/main/java/io/wyki/aggro/api/AggregationsApiImpl.java
+++ app/src/main/java/io/wyki/aggro/api/AggregationsApiImpl.java
@@ -3,12 +3,9 @@ package io.wyki.aggro.api;
 import io.wyki.aggro.app.generated.api.AggregationsApi;
 import io.wyki.aggro.app.generated.model.AggregationType;
 
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.Arrays;
 
-//@Produces(MediaType.APPLICATION_JSON)
 public class AggregationsApiImpl implements AggregationsApi {
     @Override
     public Response listAggregations() {
